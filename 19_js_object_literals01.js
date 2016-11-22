472121dcd4de3be33f97cdd8ddb7decede66d509
marebe <mark.eberhard@sinnerschrader.com>
2016-11-22 13:23:15 +0100

    default arguments

---
+++ 19_js_object_literals01.js
@@ -0,0 +1,25 @@
+function createBookShop(inventory) {
+	return {
+		// same as inventory: inventory (ie when 'key' and 'value' the same)
+		inventory,
+		// same as inventoryValue: function() { ...etc
+		inventoryValue() {
+			return this.inventory.reduce((total, book) => total + book.price, 0);
+		},
+		priceForTitle(title) {
+			return this.inventory.find(book => book.title === title).price;
+		}
+	};
+}
+
+const inventory = [
+	{ title: 'Harry Potter', price: 10 },
+	{ title: 'Eloquent Javascript', price: 15 }
+];
+
+const bookshop = createBookShop(inventory);
+
+bookshop.inventoryValue();		// 25
+bookshop.priceForTitle('Harry Potter');		//10
+
+// you're saving a few characters here
---
+++ 20_js_default_fn_arguments.js
@@ -0,0 +1,15 @@
+// here, setting method default to 'GET'
+function makeRequest(url, method = 'GET') {
+	return method;
+}
+
+// method not specified, so returns the default 'GET'
+makeRequest('google.com');
+
+// here we are setting method to 'POST', so returns 'POST'
+makeRequest('google.com', 'POST');
+
+// here we are setting to null, so returns nothing
+makeRequest('google.com', null);
+
+// note, the defaults can also be a function
